b679eee39f3facc3bc2fd045fd738f2204c7921e
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2024-09-09 11:25:19 -0500

    windows only; i will never use this in linux

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
 vim.opt.rtp:append({'~/.tmux.nvim'})
 local tmux = require('tmux')
-if vim.fn.has 'win32' == 1 then
-  vim.cmd [[source ~/vimfiles/vimrc]]
-else
-  vim.cmd [[source ~/.vim/vimrc]]
-end
+vim.cmd [[source ~/vimfiles/vimrc]]
 tmux.start()
