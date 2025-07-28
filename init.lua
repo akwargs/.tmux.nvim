bb7b6de2eea2cf5c8660ea1f59b970f2fb045dee
Anthony <135379770+akwargs@users.noreply.github.com>
2025-07-28 10:51:03 -0500

    terminal should wrap

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.cmd([[source ~/.tmux.nvim/antiphoton.vim]])
 vim.cmd([[source ~/.tmux.nvim/highlights.vim]])
+vim.opt.wrap = true
 vim.opt.rtp:append({ "~/.tmux.nvim" })
 local tmux = require("tmux")
 tmux.start()
